91ac8bdec622209d43a8d51c6405c8d686ea8f31
Nik Sofianos <nsofianos@hotmail.com>
2020-11-26 21:43:01 +0000

    protect against sql injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,17 +7,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name ILIKE $1
 ORDER BY teacher;
-`)
+`;
+const cohort = process.argv[2];
+const values = [`%${cohort}%`];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
-});
+}).catch(err => console.error('query error', err.stack));
